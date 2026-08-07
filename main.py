4ec1756d3c9e66760b733e318da3bf05243beaad
amirhosinabdoli882-cyber <amirhosinabdoli882@gmail.com>
2026-08-07 19:15:44 +0330

    Update khob3

--- main.py
+++ main.py
@@ -68,17 +68,27 @@ async def is_member(bot, user_id):
         return False
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     save_user(update.effective_user.id)
-    keyboard = [
-           if is_banned(update.effective_user.id):
+
+    if is_banned(update.effective_user.id):
         await update.message.reply_text(
             "⛔ شما از استفاده از این ربات مسدود شده‌اید."
         )
-        return 
+        return
+
+    keyboard = [
         [InlineKeyboardButton("📢 عضویت در کانال", url="https://t.me/Axyoy")],
         [InlineKeyboardButton("✅ عضو شدم", callback_data="check")]
     ]
+
+    if await is_member(context.bot, update.effective_user.id):
+        await update.message.reply_text("✅ خوش اومدی، عضویتت تأیید شد.")
+    else:
+        await update.message.reply_text(
+            "سلام 👋\n\nلطفاً ابتدا عضو کانال شوید.",
+            reply_markup=InlineKeyboardMarkup(keyboard)
+        )
 
     if await is_member(context.bot, update.effective_user.id):
         await update.message.reply_text("✅ خوش اومدی، عضویتت تأیید شد.")
